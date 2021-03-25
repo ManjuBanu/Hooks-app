b32e4b32707f8936904a9a32a06d8a33e28ee29e
v484407 <manjumala.devim@amerisourcebergen.com>
2021-03-25 12:53:00 +0530

    Toogle state with useState

--- src/AppClass.js
+++ src/AppClass.js
@@ -3,22 +3,44 @@ import React from 'react';
 class AppClass extends React.Component{
 
     state ={
-        count :0 //init
+        count :0, //init
+        isOn:false
     }
 
     incrementCount = () =>{
-        this.setState({
-            count:this.state.count +1
-        })
+        this.setState(prevState => ({
+            count:prevState.count +1
+        }));
     }
+
+    toggleLight = () =>{
+        this.setState(prevState =>({
+            isOn: !prevState.isOn
+        }))
+    }
+
+    
 
     render(){
         return(
             <React.Fragment>
-            <h1>Class!</h1>
-            <button onClick={this.incrementCount}>
-            I was clicked {this.state.count} times!!
-            </button>
+                <h1>Class!</h1>
+                <h3>Counter</h3>
+                    <button onClick={this.incrementCount}>
+                         I was clicked {this.state.count} times!!
+                    </button>
+                
+                    <h3>Toggle Light!!</h3>
+                    <div
+                    style={{
+                        height:'50px',
+                        width:'50px',
+                        background: this.state.isOn ? "yellow" :"grey",
+                        borderRadius:'50%'
+                    }}
+                    onClick={this.toggleLight}>
+                    </div>
+
             </React.Fragment>
         )
     }
--- src/AppFunc.js
+++ src/AppFunc.js
@@ -15,16 +15,46 @@ const  AppFunc = ()  => {
   //pass init state [0] to useState() it will treturn [1.current state (count)& 2.another funtion (setIncrementCount) to update this value]
   const [count, setIncrementCount] = useState(0); //0 => init
 
+  const [isOn , setIsOn] = useState(false);
+
 
  const  incrementCount = () => { 
-  setIncrementCount(count+1);
+  // setIncrementCount(count+1);
+  setIncrementCount(prevCount => prevCount+1);
+}
+
+const toggleLight = () =>{
+  setIsOn(prevIsOn => !prevIsOn )
 }
 
   return (
-    <div >
+    <React.Fragment >
       <h1>Function & Hooks!</h1>
+      <h3>Counter</h3>
       <button onClick={incrementCount}> I was clicked {count} times</button>
-    </div>
+
+
+      <h3>Toggle Light!!</h3>
+     
+      <div
+    //   src={
+    //     isOn 
+    //     ? 
+    //     'src/images/torch.png'
+    //   :
+    //   'src/images/flashlight.png'
+    // }
+      style={{
+          height:'50px',
+          width:'50px',
+          background: isOn ? "yellow" :"grey",
+          borderRadius:'50%'
+          
+      }}
+      // alt="FlashLight"
+      onClick={toggleLight}/>
+
+    </React.Fragment>
   );
 }
 
